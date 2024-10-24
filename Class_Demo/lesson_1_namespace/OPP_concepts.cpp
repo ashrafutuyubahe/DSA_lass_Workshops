58c6bdaa12480e8a0009c9bc506fefe07a497bf2
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-24 19:20:56 +0200

    asking user inputs

--- Class_Demo/lesson_1_namespace/OPP_concepts.cpp
+++ Class_Demo/lesson_1_namespace/OPP_concepts.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 
-  int  main (){
+int  main (){
 
-cout << "here is  student management system"<< endl;
+char studentName[20];
+char studentClass;
+int studentAge;
+cout << "+++++++++++++++++++++++++++++ Here is  student management system +++++++++++++++++++++++"<< endl;
 
+cin
 
 
 
@@ -16,11 +20,15 @@ cout << "here is  student management system"<< endl;
 class Student{
 
   private:
-   char  studName[20];
-  int studAge;
-  char studClass
+
+   char  studName[20]="Aisha";
+  int studAge=20;
+  char studClass='a';
 
   public:
+
+ Student(char studName,int studAge,char studClass){};
+
    Student(char studName,int studAge,char studClass)
   {
      studAge=studName;
@@ -32,24 +40,28 @@ class Student{
    void getStudName(){
     return studName;
    }
-   void getStudName(){
-    return studName;
-   }
- void getStudName(){
-    return studName;
-   }
- void getStudName(){
-    return studName;
-   }
- void getStudName(){
-    return studName;
-   }
- void getStudName(){
-    return studName;
-   }
- void getStudName(){
-    return studName;
+
+   void getStudClass(){
+    return studClass;
    }
 
+ void getStudAge(){
+    return StudAge;
+   }
+
+ void setStudName(char newStudName){
+     studName=newStudName;
+   }
+
+ void setStudAge(int newStudAge){
+     studAge=newStudAge;
+   }
+
+ void setStudClass(char newStudClass){
+     studClass=newStudClass;
+   }
+
+ 
+ 
    
 }
