95d75c9fa1020dcff5ada7490b2c76708a3b2b7e
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-10-24 19:14:27 +0200

    making new changes by making student class

--- Class_Demo/lesson_1_namespace/OPP_concepts.cpp
+++ Class_Demo/lesson_1_namespace/OPP_concepts.cpp
@@ -1,12 +1,55 @@
 #included<iostream>
-
 using namespace std;
 
 
-int main (){
+  int  main (){
+
+cout << "here is  student management system"<< endl;
 
 
 
 
     return 0;
+}
+
+
+class Student{
+
+  private:
+   char  studName[20];
+  int studAge;
+  char studClass
+
+  public:
+   Student(char studName,int studAge,char studClass)
+  {
+     studAge=studName;
+     studAge=studAge;
+     studClass=studClass;
+
+  } 
+   
+   void getStudName(){
+    return studName;
+   }
+   void getStudName(){
+    return studName;
+   }
+ void getStudName(){
+    return studName;
+   }
+ void getStudName(){
+    return studName;
+   }
+ void getStudName(){
+    return studName;
+   }
+ void getStudName(){
+    return studName;
+   }
+ void getStudName(){
+    return studName;
+   }
+
+   
 }
