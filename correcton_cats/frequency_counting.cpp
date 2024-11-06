084163ae8bd31c6970c4c7510c39bc87f645bbf3
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-11-06 11:51:41 +0200

    finished  the program(frequent calculating program)

--- correcton_cats/frequency_counting.cpp
+++ correcton_cats/frequency_counting.cpp
@@ -1,8 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <list>
+
 using namespace std;
 
-int main(){
-  
+void calculate_count(int n)
+{
 
+    int numberAray[10] = {1, 3, 5, 6, 4, 6, 4, 3, 3, 6};
+    int nummber_count = 0;
+
+    for (int number : numberAray)
+    {
+        if (number == n)
+        {
+            nummber_count += 1;
+        }
+    }
+    cout << "the number of frequency of " << n << " is: " << nummber_count << endl;
+
+     
+}
+
+int main()
+{
+
+    calculate_count(0);
     return 0;
 }
